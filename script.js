765996eb7808217f62079f5cb9b4a43a185b689a
Alex Francisco <alex2007f@gmail.com>
2021-03-27 19:24:22 -0300

    revisando

---
+++ script.js
@@ -0,0 +1,16 @@
+function upperName(name) {
+  return name.toUpperCase();
+}
+
+document.write(upperName('alex'));
+document.write('<br>')
+
+
+const upperName2 = function(name) {
+  return name.toUpperCase();
+};
+
+document.write(upperName2('francisco'));
+
+
+
