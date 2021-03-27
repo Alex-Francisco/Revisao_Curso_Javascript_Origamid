d0800607e18cd2a626523a3b283402815564d349
Alex Francisco <alex2007f@gmail.com>
2021-03-27 19:37:25 -0300

    revisando

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-function upperName(name) {
+function upperName1(name) {
   return name.toUpperCase();
 }
 
-document.write(upperName('alex'));
-document.write('<br>')
+document.write(upperName1('alex'));
+document.write('<br>');
 
 
 const upperName2 = function(name) {
@@ -11,6 +11,31 @@ const upperName2 = function(name) {
 };
 
 document.write(upperName2('francisco'));
+document.write('<br>');
 
+
+const upperName3 = name => {
+  return name.toUpperCase();
+}
+
+document.write(upperName3('silva'));
+document.write('<br>');
+
+
+
+// se tiver só uma linha retornada, não precisa passar as {} e pode tirar o return
+const upperName4 = name => name.toUpperCase();
+
+document.write(upperName4('souza'));
+document.write('<br>')
+
+
+
+const doisParametros = (p1, p2) => {
+  return p1.toUpperCase() + " " + p2.toUpperCase();
+}
+
+document.write(doisParametros('use ( )', 'quando tiver mais de 1 parâmetro'));
+document.write('<br>');
 
 
