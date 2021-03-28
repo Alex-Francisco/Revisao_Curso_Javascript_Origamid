4654acc76f7ae27ae80bfe2dcb2ddfb2d93be986
Alex Francisco <alex2007f@gmail.com>
2021-03-28 10:36:39 -0300

    revisando

--- 04-rest_e_spread/script.js
+++ 04-rest_e_spread/script.js
@@ -12,6 +12,7 @@ const numeros = [1, 2, 4, 29, 32, 2, 45, 3];
 console.log(Math.max(...numeros)); // espalha a array numeros, seria o mesmo quer passar de um em um (1, 2, 4, ...)
 
 
+
 // querySelectorAll('li') retorna uma nodeList que parece um array, mas não é
 const itens = document.querySelectorAll('li');
 
@@ -20,3 +21,33 @@ itens.forEach(item => {
 })
 
 console.log(itens);
+
+
+/*
+// map não funcionaria numa nodeList - A diferença entre forEach e map, é que a map retorna um novo array
+const itens1 = document.querySelectorAll('li');
+itens1.map(item => {
+  console.log(item);
+});
+
+console.log(itens1);
+*/
+
+
+// transformando arrayLike em uma array
+
+const itens2 = Array.from(document.querySelectorAll('li'));
+itens2.map(item => {
+  console.log(item);
+});
+
+console.log(itens2);
+
+// ou transforma em array assim
+
+const itens3 = document.querySelectorAll('li');
+[...itens].map(item => {
+  console.log(item);
+});
+
+console.log(itens3);
