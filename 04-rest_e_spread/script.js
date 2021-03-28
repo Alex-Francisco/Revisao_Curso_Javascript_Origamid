c811c5add9f0fe384735bf67552b1207afe6da53
Alex Francisco <alex2007f@gmail.com>
2021-03-28 10:28:53 -0300

    revisando

--- 04-rest_e_spread/script.js
+++ 04-rest_e_spread/script.js
@@ -10,3 +10,13 @@ showList("Origamid", "João", "Maria", 10);
 
 const numeros = [1, 2, 4, 29, 32, 2, 45, 3];
 console.log(Math.max(...numeros)); // espalha a array numeros, seria o mesmo quer passar de um em um (1, 2, 4, ...)
+
+
+// querySelectorAll('li') retorna uma nodeList que parece um array, mas não é
+const itens = document.querySelectorAll('li');
+
+itens.forEach(item => {
+  console.log(item);
+})
+
+console.log(itens);
