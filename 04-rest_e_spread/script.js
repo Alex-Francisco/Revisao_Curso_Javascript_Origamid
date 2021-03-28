e7a6bc3263279fbc8c5475f18d81ddec4670f67d
Alex Francisco <alex2007f@gmail.com>
2021-03-28 10:17:22 -0300

    revisando

--- 04-rest_e_spread/script.js
+++ 04-rest_e_spread/script.js
@@ -1,27 +1,6 @@
-
-function handleMouseMove(event) {
-  const clientX = event.clientX;
-  const clientY = event.clientY;
-  console.log(clientX, clientY)
+function showList(empresa, ...clientes) {
+  console.log(empresa); // Origamid
+  console.log(clientes); // Retorna uma array ->
 }
 
-//mousemove é um objeto com várias propriedades
-document.documentElement.addEventListener('mousemove', handleMouseMove);
-
-// desestruturando
-function handleMouseMove1(event) {
-  const {clientX, clientY, target} = event;
-  console.log(clientX, clientY);
-}
-
-//ou
-
-function handleMouseMove2({clientX, clientY}) {
-  console.log(clientX, clientY);
-}
-
-
-// pode ser feito com array
-const frutas = ["Banana", "Uva"];
-const [fruta1, fruta2] = frutas; //assim
-console.log(fruta1, fruta2);
+showList("'Origamid", "João", "Maria", 10);
