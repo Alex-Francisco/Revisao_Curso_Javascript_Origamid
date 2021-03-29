1a75e38f4825c46a5667d377f9c65e009ad30927
Alex Francisco <alex2007f@gmail.com>
2021-03-29 09:30:49 -0300

    revisando

--- 05-module/script.js
+++ 05-module/script.js
@@ -1,7 +1,12 @@
 import { areaQuadrado, perimetroQuadrado } from "./quadrado.js";
 import numeroAleatorio from "./numeroAleatorio.js";
+import Circulo from "./circulo.js";
 
 console.log(areaQuadrado(4));
 console.log(perimetroQuadrado(5));
 
 console.log(numeroAleatorio());
+
+console.log(Circulo.area(40));
+console.log(Circulo.circunferencia(52));
+console.log(Circulo.aleatorio());
