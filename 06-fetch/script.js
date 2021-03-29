d91b1f41cac2ef211cc18817accfb3023c83a59f
Alex Francisco <alex2007f@gmail.com>
2021-03-29 09:51:34 -0300

    revisando

--- 06-fetch/script.js
+++ 06-fetch/script.js
@@ -6,7 +6,13 @@ Envia requisições assíncronas para o servidor. Serve para acessarmos / escrev
 
 fetch("https://viacep.com.br/ws/01001000/json/");
 
-const requisicao = fetch("https://viacep.com.br/ws/01001000/json/").then(resposta => {
-  console.log(resposta);
-});
+const requisicao = fetch("https://viacep.com.br/ws/01001000/json/")
+  .then(resposta => {
+    return resposta.json();
+  })
+  .then(jsonBody => {
+    document.querySelector("#app").innerHTML = jsonBody.cep;
+    console.log(jsonBody);
+  });
+
 console.log(requisicao);
