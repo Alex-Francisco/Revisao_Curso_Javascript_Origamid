5b489e0ac776bcd713564b73fd6fdc9a1821ad76
Alex Francisco <alex2007f@gmail.com>
2021-04-01 11:54:23 -0300

    revisando

---
+++ 08-arrays/script.js
@@ -0,0 +1,17 @@
+// Arrays
+
+// Map, filter e reduce retornam uma array nova, não modificando a original
+
+const precos = [
+  "Crédito",
+  "R$ 200",
+  "R$ 400",
+  "Contas Pagar",
+  "R$ 300",
+  "R$ 400",
+  "Meus dados"
+];
+
+const precosFiltro = precos.filter(function(preco){
+  console.log(preco);
+});
