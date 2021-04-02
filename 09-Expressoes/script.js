91c986ad6b60982e8208ab174775d01f9f4af047
Alex Francisco <alex2007f@gmail.com>
2021-04-01 21:41:59 -0300

    revisando

--- 09-Expressoes/script.js
+++ 09-Expressoes/script.js
@@ -3,7 +3,10 @@
 const grupoA = 100;
 const grupoB = 300;
 
-const vencedor = grupoA > grupoB ? "Grupo A Ganhou" : "Grupo B Ganhou";
+const vencedor = grupoA > grupoB ? "Grupo A Ganhou" : "Grupo B Ganhou"; // ternário é uma expressão, bloco de if não 
 console.log(vencedor);
+
+const areaQuadrado = l => l * l; // <- também é uma expressão
+
 
 
