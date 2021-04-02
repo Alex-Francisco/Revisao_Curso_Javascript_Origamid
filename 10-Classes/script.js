0bf58119ddbc35d3c61a9c6078dfc4b361f79a61
Alex Francisco <alex2007f@gmail.com>
2021-04-02 14:22:07 -0300

    revisando

---
+++ 10-Classes/script.js
@@ -0,0 +1,12 @@
+// Classes
+
+const grupoA = 100;
+const grupoB = 300;
+
+const vencedor = grupoA > grupoB ? "Grupo A Ganhou" : "Grupo B Ganhou"; // ternário é uma expressão, bloco de if não 
+console.log(vencedor);
+
+const areaQuadrado = l => l * l; // <- também é uma expressão
+
+
+
